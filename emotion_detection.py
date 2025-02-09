bd778aa631c8735670787494c8b8f4bb9f8a989f
StevenSui <shuili1986@gmail.com>
2025-02-08 21:21:45 -0500

    update emotion_detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,8 @@
 # Import the requests library to handle HTTP requests
 import requests
+import json
 
-  # Define a function named emotion_detector that takes a string input (text_to_analyse)
+# Define a function named emotion_detector that takes a string input (text_to_analyse)
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     
@@ -13,6 +14,17 @@ def emotion_detector(text_to_analyse):
     
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extracting required set of emotions from the response
+    anger_score = formatted_response['emotionPredictions']['emotion']['anger']
+    disgust = formatted_response['emotionPredictions']['disgust']
+    fear = formatted_response['emotionPredictions']['fear']
+    joy = formatted_response['emotionPredictions']['joy']
+    sadness = formatted_response['emotionPredictions']['sadness']
     
+
     # Return the response text from the API
     return response.text
